59079006a8d258da5330a30add5049bd2c344ceb
Ana Edwards <edwards.ana81@gmail.com>
2022-06-04 20:02:10 +0100

    using the context

--- src/components/Layout/HeaderChartButton.js
+++ src/components/Layout/HeaderChartButton.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import styles from './HeaderChartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
-
+import CardContext from '../../store/CartContext';
 
 const HeaderChartButton = (props) => {
 
-    
+    const useCartContext = useContext(CardContext);
+
+    const numberOfCartItems = useCartContext.items.reduce((currentNumber, item) => {
+        return currentNumber + item.amount;
+    }, 0);
+
     return (
         <button className={styles.button} onClick={props.whenCartVisibilityIsClicked}> {/*We have passed down the pointer to this onClick function */}
 
@@ -15,7 +20,7 @@ const HeaderChartButton = (props) => {
             
             <span>Basket</span>
 
-            <span className={styles.badge}>3</span>                    
+            <span className={styles.badge}>{numberOfCartItems}</span>                    
                 
         </button>
     );
