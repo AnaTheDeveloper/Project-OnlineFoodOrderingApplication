2db5686bfe8b9bf254f0a41ef19eb7143fb2f1d6
Ana Edwards <edwards.ana81@gmail.com>
2022-06-05 17:23:03 +0100

    useEffect

--- src/components/Layout/HeaderChartButton.js
+++ src/components/Layout/HeaderChartButton.js
@@ -1,18 +1,39 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import styles from './HeaderChartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
 import CartContext from '../../store/CartContext';
 
 const HeaderChartButton = (props) => {
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
     const useCartContext = useContext(CartContext);
 
-    const numberOfCartItems = useCartContext.items.reduce((currentNumber, item) => {
+    const { items } = useCartContext;
+
+    const numberOfCartItems = items.reduce((currentNumber, item) => {
         return currentNumber + item.amount;
     }, 0);
 
+    const buttonClasses = `${styles.button} ${btnIsHighlighted ? styles.bump : ''}`;
+
+    useEffect(() => {
+      if (items.length === 0) {
+        return;
+      }
+      setBtnIsHighlighted(true);
+  
+      const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+  
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [items]);
+
     return (
-        <button className={styles.button} onClick={props.whenCartVisibilityIsClicked}> {/*We have passed down the pointer to this onClick function */}
+        <button className={buttonClasses} onClick={props.whenCartVisibilityIsClicked}> {/*We have passed down the pointer to this onClick function */}
 
             <span className={styles.icon}>
                 <CartIcon/>
